23ecbca00f49e38014c3fb71adcce0e3f2a46176
tomvictor <vjtomvictor@icloud.com>
2018-04-25 13:06:29 +0530

    initial commit

--- koinex.py
+++ koinex.py
@@ -1,2 +1,31 @@
+import requests
+import json
+import os
+
+API = "https://koinex.in/api/ticker"
+
 print("-----Koinex-----")
 
+r = requests.get(API)
+
+# print(r.text)
+
+
+data = r.json()
+# print(data)
+
+
+print("XRP : " + data["prices"]["XRP"])
+
+read_string = "Hey Tomy, check the crypto update,"
+
+read_string += "Ripple coin"
+read_string += data["prices"]["XRP"]
+read_string += "INR"
+
+cml = "say " + read_string
+
+os.system(cml)
+
+
+# print()
